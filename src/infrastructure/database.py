332e8fb24291ca3c16fac6e999ad8daac737902d
Dawood <dawoodkhorsandi@gmail.com>
2025-07-20 15:10:56 +0330

    add database connections

--- src/infrastructure/database.py
+++ src/infrastructure/database.py
@@ -1,30 +1,29 @@
-# app/database.py
+from contextlib import asynccontextmanager
+
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 from src.common.settings import get_settings
 
-
 settings = get_settings()
-# Retrieve DATABASE_URL from the settings object
-DATABASE_URL = settings.DATABASE_URL
 
-engine = create_async_engine(DATABASE_URL, echo=True)
-
+engine = create_async_engine(settings.postgres_dsn, future=True,
+                             echo=True)
 
 AsyncSessionLocal = sessionmaker(
-        autocommit=False,
-        autoflush=False,
-        bind=engine,
-        class_=AsyncSession,
-        expire_on_commit=False
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
 )
 
 Base = declarative_base()
 
 
-async def get_db():
-    async with AsyncSessionLocal() as session:
-        try:
-            yield session
-        finally:
-            await session.close()
+@asynccontextmanager
+async def get_session() -> AsyncSession:
+    session = AsyncSessionLocal()
+    try:
+        yield session
+    finally:
+        await session.close()
